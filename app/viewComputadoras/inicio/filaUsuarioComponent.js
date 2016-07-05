6ab5fbb5940c9b94cb9d8fa07eea4d3f69b0f6e8
Alexander Ortiz <vtacius@gmail.com>
2016-07-04 21:59:31 -0600

    Intento limpiar la "API". En computadoras, creo la que creo será, a nivel gráfico, la versión definitiva de los listados. No hay funcionalidad en los botones aún

---
+++ app/viewComputadoras/inicio/filaUsuarioComponent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('justineApp').
+component('filaUsuarios', {
+    templateUrl: 'viewComputadoras/inicio/filaUsuarioComponent.html',
+    bindings: {
+        usuario: '<'	
+    }
+});

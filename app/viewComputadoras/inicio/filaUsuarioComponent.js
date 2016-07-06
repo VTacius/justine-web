781755a271c81d4c90daf1a17e89c19c3614a84e
Alexander Ortiz <vtacius@gmail.com>
2016-07-06 09:03:00 -0600

    Hago unos simples pero importantes cambios de estilo para que el componente usuario-detalle este mejor integrado

--- app/viewComputadoras/inicio/filaUsuarioComponent.js
+++ app/viewComputadoras/inicio/filaUsuarioComponent.js
@@ -6,11 +6,13 @@ component('filaUsuarios', {
     controller: function($http){
         var ctrl = this;
         ctrl.corpus = "";
+        ctrl.muestraDetalleUsuario = false;
         ctrl.mostrarDetalleUsuario = function(){
             console.log('Así que puedo meter esta función en el botón bendito aquel');
             $http({method: 'GET', url: '/api/detalle_usuario.json'}).
                 then(function(respuesta){
                     ctrl.corpus = respuesta.data.data;
+                    ctrl.muestraDetalleUsuario = true;
                 }, function(respuesta){
                     console.log(respuesta);
                 })

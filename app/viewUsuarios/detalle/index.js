4aa3b359de6759da9b5fc593e4442a706c135d48
Alexander Ortiz <vtacius@gmail.com>
2016-07-19 09:28:25 -0600

    Actualizo el componente detalle-usuarios para que se actulice con la nueva API

--- app/viewUsuarios/detalle/index.js
+++ app/viewUsuarios/detalle/index.js
@@ -6,10 +6,6 @@ component('detalleUsuarios', {
     controller: function(){
 
         var ctrl = this;
-        console.log(ctrl.usuario); 
-        /* TODO: Sigo presentando mal los datos, hay que trabajar esta parte */
-        ctrl.grupoUsuarios = ctrl.usuarioDetalle.grupo;
-        ctrl.gruposUsuarios = ctrl.usuarioDetalle.grupos;
         
     },
     bindings: {

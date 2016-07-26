6df40b4b81369159ad42210f5a76c4f693c07bba
Alexander Ortiz <vtacius@gmail.com>
2016-07-26 12:47:04 -0600

    Doy por terminada la segunda fase del menú, que supone que todo lo demás puede empezar a estar bien, tal como se supone que debe estar

--- app/components/jt-menu/index.js
+++ app/components/jt-menu/index.js
@@ -6,8 +6,16 @@ component('jtMenu', {
     templateUrl: 'components/jt-menu/index.html',
     controller: function(__ENV){
         var ctrl = this;
-       
+        
+        /* Obtenemos datos, principalmente de la sesión */       
         ctrl.titulo = __ENV.titulo;
         ctrl.sesion = __ENV.sesion;
+
+        /* Empezamos los comportamientos */
+        ctrl.mostrarPanel = false;
+
+        ctrl.muestraPanel = function(estado){
+            ctrl.mostrarPanel = estado;
+        }
     }
 })

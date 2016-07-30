f58d811e64a8e61a7530cbbddd7b44085a879fff
Alexander Ortiz <vtacius@gmail.com>
2016-07-30 10:55:48 -0600

    o y ou ya se están comportando mejor, en cuanto a la validacion required ya los tengo. Comentarizo y hago una primera revisión del estado actual de las validaciones del formulario

--- app/viewUsuarios/creacion/formulario-usuario/index.js
+++ app/viewUsuarios/creacion/formulario-usuario/index.js
@@ -7,9 +7,23 @@ component('formularioUsuario', {
     controller: function ($http) {
         var ctrl = this;
 
+        /* Lista de establecimientos que usamos para las sugerencias en Establecimientos (o) */
         ctrl.establecimientos = [];
+
+        /* Por defecto, el componente Oficina (ou) no es obligatorio pues no sabemos si hay datos disponibles para sugerir */
+        ctrl.requerirOficina = false;
+        
+        /* Es capaz de almacenar el objecto seleccionado en Establecimiento (o), o de inicializar su valor en el formulario */
+        ctrl.establecimiento = {
+            "id":"1038",
+            "nombre":"Secretaría de Estado SS Ministerio de Salud"
+        };
+
+        /* Inicializa | almacena el objeto seleccionado en Oficina (ou) */
+        ctrl.oficina = {}
+
        
-        /* Si el formulario necesita datos para mostrar datos en sus formularios, que se los consiga el componente */
+        /* Inicializando el componente */
         ctrl.$onInit = function(){
 
             /* Obtenemos los establecimiento con los que vamos a llenar las sugerencias para Establecimientos (o) */
@@ -20,33 +34,39 @@ component('formularioUsuario', {
                     console.log("Hay un problema con el servidor en este punto");
                 });
 
-            /* Obtenemos usuario desde componente padre */
+            /* Obtenemos usuario desde componente padre, mediante el binding corpus */
             ctrl.usuario = ctrl.corpus; 
         
-            /* Copiamos el modelo para resetear los valores al original */
+            /* Almacenamos el modelo, así podremos resetear el formulario de ser necesario */
             ctrl.usuarioOriginal = angular.copy(ctrl.usuario);
         };
+       
+        /*
+         * Empiezan las operaciones helpers
+         *
+         * /
         
-        /* Empiezan las operaciones helpers */
-        /* Usada en seleccionaEstablecimiento, entre otras cosas porque odio ver tanto identado */
+        /* Usada en seleccionaEstablecimiento, llenamos las sugerencias de oficinas con las propias del establecimiento */
         var obtenerOficinas = function(oficina){
             $http({method: 'GET', url: '/api/helpers_oficinas/' + oficina + '.json'}).
                 then(function(respuesta){
                     ctrl.oficinas = respuesta.data;
-                    if (!ctrl.oficinas){
-                        ctrl.requerirOficina = false;
-                    }
+                    /* Sólo si hay datos de la oficina tiene sentido que llenar el componente sea obligatorio */
+                    ctrl.requerirOficina = (ctrl.oficinas.length > 0) ? true : false;
                 }, function(respuesta){
+                    /* Hubo un error, creo que algo podríamos hacer en este punto */
                     ctrl.requerirOficina = false;
-                    ctrl.oficinas = [];
                 });
 
         };
 
 
-        /* Esto se corresponde con la funcionalidad de los componentes */
+        /*
+         * Empezamos a trabajar en la funcionalidad de los componentes
+         *
+         */
 
-        /* Hay controles que se muestran según {{Sigo sin ponerne de acuerdo sobre que lo controlará}} */
+        /* Hay controles que se muestran según el binding accion, que especifica para que se usará el formulario */
         ctrl.mostrarCampos = function(clave){
             if (ctrl.accion === "creacion" && (clave ==="zimbra" || clave ==="samba" || clave ==="username" )){
                 return true;
@@ -65,12 +85,7 @@ component('formularioUsuario', {
             ctrl.usuario[modelo]['estado'] = estado;
         }
         
-        /* La idea es guardar el establecimiento en un auxiliar, como hice antes */
-        ctrl.establecimiento = {
-            "id":"343",
-            "nombre":"UCSF Sensuntepeque CA"
-        };
-        /* Un callback a angucomplete-alt de establecimiento */
+        /* Callback para selected-object de angucomplete-alt que permite tratar mejor al elemento seleccionado en Establecimiento (o)  */
         ctrl.seleccionaEstablecimiento = function(seleccionado){
             if (seleccionado){
                 ctrl.establecimiento = seleccionado.originalObject;
@@ -78,10 +93,10 @@ component('formularioUsuario', {
             };
         };
 
-        /* Un callaback a angucomplete-alt de oficina */
+        /* Callback para select-object de angucomplete-alt para elemento seleccionado de Oficina (ou) */
         ctrl.seleccionaOficina = function(seleccionado){
             if (seleccionado){
-                ctrl.usuario.ou = seleccionado.originalObject.nombre;
+                ctrl.oficina = seleccionado.originalObject.nombre;
             };
         };
  
@@ -91,10 +106,15 @@ component('formularioUsuario', {
             ctrl.usuario = angular.copy(ctrl.usuarioOriginal);
         };
 
-        /* Funcionalidad asociada con submit del formulario, que requiere enviar los datos del formulario hacia un nivel padre */
+        /* 
+         * Funcionalidad asociada con submit del formulario, que requiere enviar los datos del formulario hacia un nivel padre 
+         * También se ocupa de asociar ctrl.establecimiento y ctrl.oficina al objeto que enviamos como usuario
+         *
+         * */
         ctrl.enviar = function(usuario){
             console.log('Envío desde el componente que especifica al formulario');
             usuario.o = ctrl.establecimiento;
+            usuario.ou = ctrl.oficina;
             console.log(usuario);
             ctrl.ejecucion({'corpus': usuario});
         }
